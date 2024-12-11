d46d5347ddd3089ac764113c6e823a1c84f52f21
Diego Caucota <diegocaucota@gmail.com>
2024-12-11 17:48:20 -0300

    add: Api ProductController functions

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Rutas protegidas para Productos
 Route::get('products', [ProductController::class, 'index']);
-
+// Filtrar los Productos por Color
 Route::get('products/{color}/color', [ProductController::class, 'filterProductsByColor']);
-
+// Filtrar los Productos por Tamaño
 Route::get('products/{size}/size', [ProductController::class, 'filterProductsBySize']);
+
+Route::get('products/{searchTerm}/find', [ProductController::class, 'searchProductsByTerm']);
+
+Route::get('products/{product}/show', [ProductController::class, 'show']);
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
